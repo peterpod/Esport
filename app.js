150cb01316835acf17b34ecf2cddeb938a250a67
Peter Pod <peterpod@hotmail.com>
2014-12-01 10:52:04 -0500

    changing server.js naming

--- app.js
+++ app.js
@@ -1,105 +1,73 @@
-var express = require('express'),
-    morgan  = require('morgan'),
-    path = require('path');
+var express = require("express");
+var morgan = require("morgan");
+var sportRoutes = require('./routes/sportRoutes'); 
+var dbRoutes = require('./routes/dbRoutes'); 
+var app = express();
+var session = require('express-session');
 
-// Create a class that will be our main application
-var SimpleStaticServer = function() {
+app.set('trust proxy', 1) // trust first proxy
 
-  // set self to the scope of the class
-  var self = this;  
-  
-  self.app = express();
-  //	self.app.use(connect(connect.basicAuth('j', 'jmjm')))
-  self.app.use(morgan('[:date] :method :url :status'));	// Log requests
-  self.app.use(express.static(path.join(__dirname, 'public')));	// Process static files
+app.use(session({
+  secret: '1234567890QWERTY',
+  resave: false,
+  saveUninitialized: true
+}))
 
-  self.app.set('trust proxy', 1) // trust first proxy
+// -- This code is being used to test whether or not my
+// -- sessions are being created properly. You can navigate to /test1 and 
+// -- you should receive back your session.user_id. I took this code from professor Mertz
+app.get('/test1', function(req, res) {
+	req.session.user_id = 1;
+	return res.redirect('/test2');
+});
 
-  self.app.use(session({
-    secret: '1234567890QWERTY',
-    resave: false,
-    saveUninitialized: true
-  }))
+app.get('/test2', function(req, res) {
+	return res.send(req.session.user_id);
+});
 
-  // -- This code is being used to test whether or not my
-  // -- sessions are being created properly. You can navigate to /test1 and 
-  // -- you should receive back your session.user_id. I took this code from professor Mertz
-  self.app.get('/test1', function(req, res) {
-    req.session.user_id = 1;
-    return res.redirect('/test2');
-  });
+app.set('views', __dirname + '/views');
+// Define the view (templating) engine
+app.set('view engine', 'ejs');
+app.use(morgan('tiny'));	// Log requests
 
-  self.app.get('/test2', function(req, res) {
-    return res.send(req.session.user_id);
-  });
+app.use('/', express.static(__dirname + '/public'));
 
-  self.app.set('views', __dirname + '/views');
-  // Define the view (templating) engine
-  self.app.set('view engine', 'ejs');
-  self.app.use(morgan('tiny'));  // Log requests
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/login.html');
+});
 
-  self.app.use('/', express.static(__dirname + '/public'));
+app.get('/home', function(req, res) {
+	//-- testing to see if session cookie is generated res.send(req.session);
+    res.sendFile(__dirname + '/public/home.html');
+});
 
-  self.app.get('/', function(req, res) {
-      res.sendFile(__dirname + '/public/login.html');
-  });
+app.get('/signup', function(req, res) {
+    res.sendFile(__dirname + '/public/signup.html');
+});
 
-  self.app.get('/home', function(req, res) {
-    //-- testing to see if session cookie is generated res.send(req.session);
-      res.sendFile(__dirname + '/public/home.html');
-  });
+// -- User Mongo Routes
+app.put('/:collection/:username/:password/:firstname/:lastname', dbRoutes.putUser);
+app.get('/user/edit/:username/:password/:firstname/:lastname', dbRoutes.loadUserEdit);
+app.get('/users', dbRoutes.getUser);
+app.post('/:collection/:username/:password/:firstname/:lastname', dbRoutes.postUser);
+app.delete('/:collection/:firstname/:lastname', dbRoutes.deleteUser);
 
-  self.app.get('/signup', function(req, res) {
-      res.sendFile(__dirname + '/public/signup.html');
-  });
+// -- NBA Routes
+app.get('/nba', sportRoutes.loadNBAPage);
+app.get('/nba/boxscore/:gameID', sportRoutes.getBoxScore);
+app.get('/nba/statistics/:gameID', sportRoutes.getGameScoreAndStats);
+app.get('/nba/standings', sportRoutes.getNBAStandings);
 
-  // -- User Mongo Routes
-  self.app.put('/:collection/:username/:password/:firstname/:lastname', dbRoutes.putUser);
-  self.app.get('/user/edit/:username/:password/:firstname/:lastname', dbRoutes.loadUserEdit);
-  self.app.get('/users', dbRoutes.getUser);
-  self.app.post('/:collection/:username/:password/:firstname/:lastname', dbRoutes.postUser);
-  self.app.delete('/:collection/:firstname/:lastname', dbRoutes.deleteUser);
+// -- NFL Routes
+app.get('/nfl', sportRoutes.loadNFLPage);
+app.get('/nfl/stats/:away/:home', sportRoutes.getGameStats);
+app.get('/nfl/summary/:away/:home', sportRoutes.getGameSummary);
 
-  // -- NBA Routes
-  self.app.get('/nba', sportRoutes.loadNBAPage);
-  self.app.get('/nba/boxscore/:gameID', sportRoutes.getBoxScore);
-  self.app.get('/nba/statistics/:gameID', sportRoutes.getGameScoreAndStats);
-  self.app.get('/nba/standings', sportRoutes.getNBAStandings);
-
-  // -- NFL Routes
-  self.app.get('/nfl', sportRoutes.loadNFLPage);
-  self.app.get('/nfl/stats/:away/:home', sportRoutes.getGameStats);
-  self.app.get('/nfl/summary/:away/:home', sportRoutes.getGameSummary);
-
-  // -- NHL Routes
-  self.app.get('/nhl', sportRoutes.loadNHLPage);
-  self.app.get('/nhl/boxscore/:gameID', sportRoutes.getNHLBoxScore);
-  self.app.get('/nhl/standings', sportRoutes.getNHLStandings);
+// -- NHL Routes
+app.get('/nhl', sportRoutes.loadNHLPage);
+app.get('/nhl/boxscore/:gameID', sportRoutes.getNHLBoxScore);
+app.get('/nhl/standings', sportRoutes.getNHLStandings);
 
 
-  // Start the server (starts up the sample application).
-  self.start = function() {
-    /*
-     * OpenShift will provide environment variables indicating the IP 
-     * address and PORT to use.  If those variables are not available
-     * (e.g. when you are testing the application on your laptop) then
-     * use default values of localhost (127.0.0.1) and 33333 (arbitrary).
-     */
-    self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-    self.port      = process.env.OPENSHIFT_NODEJS_PORT || 33333;
-
-    //  Start listening on the specific IP and PORT
-    self.app.listen(self.port, self.ipaddress, function() {
-      console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-    });
-  };
-}; 
-
-
-/**
- *  main():  Main code.
- */
-var sss = new SimpleStaticServer();
-sss.start();
-
+app.listen(50000);
+console.log("Server listening at http://localhost:50000/");
--- server.js
+++ server.js
@@ -1,73 +1,105 @@
-var express = require("express");
-var morgan = require("morgan");
-var sportRoutes = require('./routes/sportRoutes'); 
-var dbRoutes = require('./routes/dbRoutes'); 
-var app = express();
-var session = require('express-session');
+var express = require('express'),
+    morgan  = require('morgan'),
+    path = require('path');
 
-app.set('trust proxy', 1) // trust first proxy
+// Create a class that will be our main application
+var SimpleStaticServer = function() {
 
-app.use(session({
-  secret: '1234567890QWERTY',
-  resave: false,
-  saveUninitialized: true
-}))
+  // set self to the scope of the class
+  var self = this;  
+  
+  self.app = express();
+  //	self.app.use(connect(connect.basicAuth('j', 'jmjm')))
+  self.app.use(morgan('[:date] :method :url :status'));	// Log requests
+  self.app.use(express.static(path.join(__dirname, 'public')));	// Process static files
 
-// -- This code is being used to test whether or not my
-// -- sessions are being created properly. You can navigate to /test1 and 
-// -- you should receive back your session.user_id. I took this code from professor Mertz
-app.get('/test1', function(req, res) {
-	req.session.user_id = 1;
-	return res.redirect('/test2');
-});
+  self.app.set('trust proxy', 1) // trust first proxy
 
-app.get('/test2', function(req, res) {
-	return res.send(req.session.user_id);
-});
+  self.app.use(session({
+    secret: '1234567890QWERTY',
+    resave: false,
+    saveUninitialized: true
+  }))
 
-app.set('views', __dirname + '/views');
-// Define the view (templating) engine
-app.set('view engine', 'ejs');
-app.use(morgan('tiny'));	// Log requests
+  // -- This code is being used to test whether or not my
+  // -- sessions are being created properly. You can navigate to /test1 and 
+  // -- you should receive back your session.user_id. I took this code from professor Mertz
+  self.app.get('/test1', function(req, res) {
+    req.session.user_id = 1;
+    return res.redirect('/test2');
+  });
 
-app.use('/', express.static(__dirname + '/public'));
+  self.app.get('/test2', function(req, res) {
+    return res.send(req.session.user_id);
+  });
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/login.html');
-});
+  self.app.set('views', __dirname + '/views');
+  // Define the view (templating) engine
+  self.app.set('view engine', 'ejs');
+  self.app.use(morgan('tiny'));  // Log requests
 
-app.get('/home', function(req, res) {
-	//-- testing to see if session cookie is generated res.send(req.session);
-    res.sendFile(__dirname + '/public/home.html');
-});
+  self.app.use('/', express.static(__dirname + '/public'));
 
-app.get('/signup', function(req, res) {
-    res.sendFile(__dirname + '/public/signup.html');
-});
+  self.app.get('/', function(req, res) {
+      res.sendFile(__dirname + '/public/login.html');
+  });
 
-// -- User Mongo Routes
-app.put('/:collection/:username/:password/:firstname/:lastname', dbRoutes.putUser);
-app.get('/user/edit/:username/:password/:firstname/:lastname', dbRoutes.loadUserEdit);
-app.get('/users', dbRoutes.getUser);
-app.post('/:collection/:username/:password/:firstname/:lastname', dbRoutes.postUser);
-app.delete('/:collection/:firstname/:lastname', dbRoutes.deleteUser);
+  self.app.get('/home', function(req, res) {
+    //-- testing to see if session cookie is generated res.send(req.session);
+      res.sendFile(__dirname + '/public/home.html');
+  });
 
-// -- NBA Routes
-app.get('/nba', sportRoutes.loadNBAPage);
-app.get('/nba/boxscore/:gameID', sportRoutes.getBoxScore);
-app.get('/nba/statistics/:gameID', sportRoutes.getGameScoreAndStats);
-app.get('/nba/standings', sportRoutes.getNBAStandings);
+  self.app.get('/signup', function(req, res) {
+      res.sendFile(__dirname + '/public/signup.html');
+  });
 
-// -- NFL Routes
-app.get('/nfl', sportRoutes.loadNFLPage);
-app.get('/nfl/stats/:away/:home', sportRoutes.getGameStats);
-app.get('/nfl/summary/:away/:home', sportRoutes.getGameSummary);
+  // -- User Mongo Routes
+  self.app.put('/:collection/:username/:password/:firstname/:lastname', dbRoutes.putUser);
+  self.app.get('/user/edit/:username/:password/:firstname/:lastname', dbRoutes.loadUserEdit);
+  self.app.get('/users', dbRoutes.getUser);
+  self.app.post('/:collection/:username/:password/:firstname/:lastname', dbRoutes.postUser);
+  self.app.delete('/:collection/:firstname/:lastname', dbRoutes.deleteUser);
 
-// -- NHL Routes
-app.get('/nhl', sportRoutes.loadNHLPage);
-app.get('/nhl/boxscore/:gameID', sportRoutes.getNHLBoxScore);
-app.get('/nhl/standings', sportRoutes.getNHLStandings);
+  // -- NBA Routes
+  self.app.get('/nba', sportRoutes.loadNBAPage);
+  self.app.get('/nba/boxscore/:gameID', sportRoutes.getBoxScore);
+  self.app.get('/nba/statistics/:gameID', sportRoutes.getGameScoreAndStats);
+  self.app.get('/nba/standings', sportRoutes.getNBAStandings);
+
+  // -- NFL Routes
+  self.app.get('/nfl', sportRoutes.loadNFLPage);
+  self.app.get('/nfl/stats/:away/:home', sportRoutes.getGameStats);
+  self.app.get('/nfl/summary/:away/:home', sportRoutes.getGameSummary);
+
+  // -- NHL Routes
+  self.app.get('/nhl', sportRoutes.loadNHLPage);
+  self.app.get('/nhl/boxscore/:gameID', sportRoutes.getNHLBoxScore);
+  self.app.get('/nhl/standings', sportRoutes.getNHLStandings);
 
 
-app.listen(50000);
-console.log("Server listening at http://localhost:50000/");
+  // Start the server (starts up the sample application).
+  self.start = function() {
+    /*
+     * OpenShift will provide environment variables indicating the IP 
+     * address and PORT to use.  If those variables are not available
+     * (e.g. when you are testing the application on your laptop) then
+     * use default values of localhost (127.0.0.1) and 33333 (arbitrary).
+     */
+    self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+    self.port      = process.env.OPENSHIFT_NODEJS_PORT || 33333;
+
+    //  Start listening on the specific IP and PORT
+    self.app.listen(self.port, self.ipaddress, function() {
+      console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+    });
+  };
+}; 
+
+
+/**
+ *  main():  Main code.
+ */
+var sss = new SimpleStaticServer();
+sss.start();
+
