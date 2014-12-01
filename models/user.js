d06dec5b3c0d269dc62c2012405450908cdf997f
Peter Pod <peterpod@hotmail.com>
2014-12-01 15:30:08 -0500

    changed url for mongo

--- models/user.js
+++ models/user.js
@@ -6,9 +6,17 @@ var mongoClient = require('mongodb').MongoClient;
  * The typical mongodb port is 27012
  * The path part (here "fallTest") is the name of the databas
  */
-//var url = 'mongodb://admin:3LB-WfVvI38W@'+process.env.OPENSHIFT_MONGODB_DB_HOST+':33333/esport'
-var url = 'mongodb://admin:3LB-WfVvI38W@127.1.244.2:33333/esport'
-//var url = 'mongodb://localhost:27017/Esport';
+var url = 'mongodb://localhost:27017/Esport';
+//var url = 'mongodb://admin:3LB-WfVvI38W@127.1.244.2:33333/esport'
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  url = 'mongodb://'+ process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+console.log(url);
 var mongoDB; // The connected database
 // Use connect method to connect to the Server
 mongoClient.connect(url, function(err, db) {
